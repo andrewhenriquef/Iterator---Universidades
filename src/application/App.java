c78c0ee80d2163b956b441c52aa232375d98523f
Andrew Henrique <andrewhenriquef@gmail.com>
2016-03-24 10:54:05 -0300

    app main functional now

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,18 @@
 package application;
 
+import listagem.ListaAlunosUfscar;
+import listagem.ListaGeral;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ListaAlunosUfscar listaUfscar = new ListaAlunosUfscar();
+		ListaGeral listaGeral = new ListaGeral();
+		
+		listaGeral.imprimeAlunos(listaUfscar.criaIterator());
+		
 
 	}
 
